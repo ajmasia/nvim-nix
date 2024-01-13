b848595b3aa0362f6d08d1c73b7ca1685737323c
Anto io José Masiá <dev@ajmasia.me>
2024-01-13 18:55:18 +0100

    use new plugins

--- module/commons/default.nix
+++ module/commons/default.nix
@@ -2,6 +2,9 @@
   imports = [
     ./lsp
     ./treesitter
+    ./telescope
+    ./trouble
+    ./git
   ];
 }
 
--- module/default.nix
+++ module/default.nix
@@ -3,6 +3,9 @@
     ./config
     ./commons
     ./ui/themes/catppuccin.nix
+    ./ui/dashboard
+    ./ui/lualine
+    ./ui/whichkey
 
     ./lang/lua.nix
     ./lang/tsserver.nix
