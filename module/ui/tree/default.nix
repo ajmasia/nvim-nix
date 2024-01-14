9a86b7a4c38b5cdfd6d3a18673b0183acbc1dc44
Anto io José Masiá <dev@ajmasia.me>
2024-01-14 17:48:15 +0100

    add new ui plugins

---
+++ module/ui/tree/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  extraPlugins = with pkgs.vimPlugins; [
+    nvim-web-devicons
+  ];
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = ":Neotree action=focus reveal toggle<CR>";
+      options = {
+        silent = true;
+        desc = "File Explorer (root)";
+      };
+    }
+  ];
+
+  plugins = {
+
+    neo-tree = {
+      enable = true;
+
+      closeIfLastWindow = true;
+      window = {
+        width = 30;
+        autoExpandWidth = true;
+      };
+    };
+  };
+}
--- module/ui/whichkey/default.nix
+++ module/ui/whichkey/default.nix
@@ -1,23 +1,24 @@
 { ... }:
 
 {
-    plugins = {
-      which-key = {
-        enable = true;
+  plugins = {
+    which-key = {
+      enable = true;
 
-        plugins.spelling.enabled = true;
+      plugins.spelling.enabled = true;
 
-        registrations = {
-          "<leader>f" = { name = "file"; };
-          "<leader>s" = { name = "search"; };
-          "<leader>sr" = { name = "replace"; };
-          "<leader>w" = { name = "windows"; };
-          "<leader>b" = { name = "buffers"; };
-          "<leader>x" = { name = "quickfix"; };
-          "]" = "+next";
-          "[" = "+prev";
-        };
+      registrations = {
+        "<leader>f" = { name = "file"; };
+        "<leader>s" = { name = "search"; };
+        "<leader>sr" = { name = "replace"; };
+        "<leader>w" = { name = "windows"; };
+        "<leader>b" = { name = "buffers"; };
+        "<leader>x" = { name = "quickfix"; };
+        "<leader>c" = { name = "code"; };
+        "]" = "+next";
+        "[" = "+prev";
       };
+    };
 
-      };
-  }
+  };
+}
