52166c9230b43434f82a39e1df25d6e6347e2526
Anto io José Masiá <dev@ajmasia.me>
2024-01-14 20:04:57 +0100

    add extra packages needed

--- module/commons/default.nix
+++ module/commons/default.nix
@@ -11,6 +11,12 @@
     ./spectre
   ];
 
+  extraPackages = with pkgs; [
+    ripgrep
+    lazygit
+    fzf
+  ];
+
   # feature that enhances the way Neovim loads and executes Lua modules, 
   # offering improved performance and flexibility.
   luaLoader.enable = true;
